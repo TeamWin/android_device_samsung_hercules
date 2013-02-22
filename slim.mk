9c2dcb9215bc3cbf16eaa8d317fd99ea79834513
prbassplayer <prbassplayer@gmail.com>
2013-02-22 20:34:52 +0100

    Initial bring up for hercules, Thanks CM!
    Patch Set #2: Thanks CM
    Patch Set #3: Fixed slim.mk
    Patch Set #4: Cleaned up slim.mk and board config, made inherint a must no if
    
    Change-Id: Ic4e8ed7b9dcf38c63a1419049ecdb52c10dd172e

--- slim.mk
+++ slim.mk
@@ -1,10 +1,7 @@
 $(call inherit-product, device/samsung/hercules/full_hercules.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := T989
-
-# Preload bootanimation
-TARGET_BOOTANIMATION_PRELOAD := true
+PRODUCT_RELEASE_NAME := t989
 
 # Inherit some common slim stuff.
 $(call inherit-product, vendor/slim/config/gsm.mk)
@@ -18,17 +15,20 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 # Inherit device settings
 $(call inherit-product, vendor/slim/config/common_sgs.mk)
 
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
 #copy boot animation
-PRODUCT_COPY_FILES +=  \
+PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
 #copy 00check
 PRODUCT_COPY_FILES += \
-	vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
+    vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.0.4/IMM76D/UVLH1:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.0.4 IMM76D UVLH1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.0.4/IMM76D/UVLI4:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.0.4 IMM76D UVLI4 release-keys"
 
-#TARGET_BOOTANIMATION_NAME := vertical-800x1280
+TARGET_BOOTANIMATION_NAME := 480
 
 PRODUCT_NAME := slim_hercules
 PRODUCT_DEVICE := hercules

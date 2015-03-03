2a23f0bd1f467f7b10388a928299c95ff04b484a
Dan Cartier <nospamdan11@gmail.com>
2015-03-02 20:36:30 -0500

    hercules: Fixup candy makefile.

--- candy5.mk
+++ candy5.mk
@@ -14,5 +14,5 @@ $(call inherit-product, vendor/candy5/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.3/JZO54K/T989UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.3 IMM76D UVMC6 release-keys"
 
 # Device identifier
-PRODUCT_NAME := slim_hercules
+PRODUCT_NAME := candy5_hercules
 PRODUCT_DEVICE := hercules

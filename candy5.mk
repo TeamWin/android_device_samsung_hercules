dde9676f3204646f044c9ce075299e3ee6bc772f
Dan Cartier <nospamdan11@gmail.com>
2015-03-22 21:34:00 -0400

    hercules: Fix up deps and makefile for Candy5

--- candy5.mk
+++ candy5.mk
@@ -1,24 +1,18 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 480
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/hercules/full_hercules.mk)
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/candy5/config/nfc_enhanced.mk)
+
+# Inherit some common Candy5 stuff.
+$(call inherit-product, vendor/candy5/config/common_full_phone.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := hercules
 
 # Device identifier
-PRODUCT_NAME := slim_hercules
+PRODUCT_NAME := candy5_hercules
 PRODUCT_DEVICE := hercules
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:5.0.2/LRX22G/T989UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 5.0.2 LRX22G UVMC6 release-keys"
+
